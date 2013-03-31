5547c01cf444954aabd966e7647edf11c4242d77
Lars Tackmann <ltackmann@gmail.com>
2013-03-31 22:32:15 +0200

    update meta

--- lib/dice.dart
+++ lib/dice.dart
@@ -2,14 +2,13 @@
 // for details. All rights reserved. Use of this source code is governed 
 // by a Apache license that can be found in the LICENSE file.
 
-/**
- * Lightweight dependency injection framework for Dart.
- */
+/** Lightweight dependency injection framework for Dart. */
 library dice;
 
-import "package:meta/meta.dart";
 import "dart:async";
 import "dart:mirrors";
+
+import "package:meta/meta.dart";
 
 part "src/binder.dart";
 part "src/injector.dart";
