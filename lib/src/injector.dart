e01a5be76d887f48bc46ee434f02c7add4b9ca48
Lars Tackmann <ltackmann@gmail.com>
2013-06-25 11:46:13 +0200

    better description for inject annotation #5

--- lib/src/injector.dart
+++ lib/src/injector.dart
@@ -109,7 +109,7 @@ class InjectorImpl implements Injector {
       method.parameters.where(injectable);
 }
 
-/** Annotation to inject classes */
+/** Used to annotate constructors, methods and fields of your classes where [Injector] should resolve values */
 class Inject {
   const Inject({this.name:null});
   
