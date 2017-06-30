5660413fd4bc5fe04b02273b0fb34b7adfe89bd9
Lars Tackmann <ltackmann@gmail.com>
2017-06-30 13:04:07 +0200

    delete old unused sample code

--- test/dice_test.dart
+++ test/dice_test.dart
@@ -1,7 +1,6 @@
 // Copyright (c) 2013, the project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed
 // by a Apache license that can be found in the LICENSE file.
-//@TestOn("chrome")
 
 library dice_test;
 
